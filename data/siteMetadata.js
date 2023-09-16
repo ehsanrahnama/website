a8d565045be6c8a0de0081e0a194d2f5a83bc0ed
e.ahanj <eng.ahanj@gmail.com>
2023-09-16 15:52:55 +0330

    change templete

---
+++ data/siteMetadata.js
@@ -0,0 +1,80 @@
+const siteMetadata = {
+  title: {
+    en: 'Marcelo Formentão – Software Engineer',
+    pt: 'Marcelo Formentão – Engenheiro de Software',
+  },
+  author: 'Marcelo Formentão',
+  headerTitle: { en: 'Personal Website', pt: 'Site Pessoal' },
+  description: {
+    en: 'A Software Engineer that code for passion and design for fun.',
+    pt: 'A Software Engineer that code for passion and design for fun.',
+  },
+  language: 'en-us',
+  siteUrl: 'https://marceloformentao.dev',
+  siteRepo: 'https://github.com/marceloavf/marceloformentao.dev',
+  siteLogo: '/static/images/logo.png',
+  image: '/static/images/avatar.png',
+  socialBanner: '/static/images/twitter-card.png',
+  email: 'me@marceloformentao.dev',
+  github: 'https://github.com/marceloavf',
+  twitter: 'https://twitter.com/marceloavf',
+  linkedin: 'https://www.linkedin.com/in/marceloavf/',
+  instagram: 'https://www.instagram.com/marceloavf/',
+  locale: 'en-US',
+  analytics: {
+    // supports plausible, simpleAnalytics, umami or googleAnalytics
+    plausibleDataDomain: 'marceloformentao.dev',
+    simpleAnalytics: false,
+    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    googleAnalyticsId: 'G-TPC5EVVJLG',
+  },
+  newsletter: {
+    // supports mailchimp, buttondown, convertkit
+    // Please add your .env file and modify it according to your selection
+    provider: '',
+  },
+  comment: {
+    // Select a provider and use the environment variables associated to it
+    // https://vercel.com/docs/environment-variables
+    provider: '', // supported providers: giscus, utterances, disqus
+    giscusConfig: {
+      // Visit the link below, and follow the steps in the 'configuration' section
+      // https://giscus.app/
+      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      mapping: 'pathname', // supported options: pathname, url, title
+      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+      metadata: '0',
+      // theme example: light, dark, dark_dimmed, dark_high_contrast
+      // transparent_dark, preferred_color_scheme, custom
+      theme: 'light',
+      // theme when dark mode
+      darkTheme: 'transparent_dark',
+      // If the theme option above is set to 'custom`
+      // please provide a link below to your custom theme css file.
+      // example: https://giscus.app/themes/custom_example.css
+      themeURL: '',
+    },
+    utterancesConfig: {
+      // Visit the link below, and follow the steps in the 'configuration' section
+      // https://utteranc.es/
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+      issueTerm: '', // supported options: pathname, url, title
+      label: '', // label (optional): Comment 💬
+      // theme example: github-light, github-dark, preferred-color-scheme
+      // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
+      theme: '',
+      // theme when dark mode
+      darkTheme: '',
+    },
+    disqusConfig: {
+      // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
+      shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
+    },
+  },
+}
+
+module.exports = siteMetadata
--- next.config.js
+++ next.config.js
@@ -1,6 +1,47 @@
-/** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const withPlugins = require('next-compose-plugins')
+
+const withTM = require('next-transpile-modules')([
+  'three',
+  'react-spring',
+  '@react-spring/three',
+  '@react-spring/web',
+  '@react-three/fiber',
+  '@react-three/postprocessing',
+  '@react-three/drei',
+])
+
+const nextTranslate = require('next-translate')
+
 const nextConfig = {
+  compress: true,
   reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  eslint: {
+    dirs: ['pages', 'components', 'lib', 'layouts', 'scripts', 'context'],
+  },
+  webpack: (config, { dev, isServer }) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      exclude: /node_modules/,
+      use: ['file-loader'],
+    })
+
+    config.module.rules.push({
+      test: /react-spring/,
+      sideEffects: true,
+    })
+
+    return config
+  },
 }
 
-module.exports = nextConfig
+module.exports = withPlugins([[nextTranslate], [withBundleAnalyzer], [withTM]], nextConfig)
