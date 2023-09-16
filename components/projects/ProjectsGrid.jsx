ac9edf3b24286be70ad2821822640e1419e5faed
e.ahanj <eng.ahanj@gmail.com>
2023-09-16 19:31:31 +0330

    some change

--- components/projects/ProjectsGrid.jsx
+++ components/projects/ProjectsGrid.jsx
@@ -5,36 +5,15 @@ import { projectsData } from "../../data/projectsData";
 import ProjectsFilter from "./ProjectsFilter";
 
 function ProjectsGrid() {
-  const [searchProject, setSearchProject] = useState();
-  const [selectProject, setSelectProject] = useState();
-
-  // @todo - To be fixed
-  // const searchProjectsByTitle = projectsData.filter((item) => {
-  // 	const result = item.title
-  // 		.toLowerCase()
-  // 		.includes(searchProject.toLowerCase())
-  // 		? item
-  // 		: searchProject == ''
-  // 		? item
-  // 		: '';
-  // 	return result;
-  // });
-
-  const selectProjectsByCategory = projectsData.filter((item) => {
-    let category =
-      item.category.charAt(0).toUpperCase() + item.category.slice(1);
-    return category.includes(selectProject);
-  });
-
   return (
-    <section className="py-5 sm:py-10 mt-5 sm:mt-10">
+    <section className="py-5 mt-10 min-h-screen">
       <div className="text-center">
         <p className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light">
           Projects portfolio
         </p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-5">
+      <div className="mt-10">
         {projectsData.map((project, index) => (
           <ProjectSingle key={index} {...project} />
         ))}
