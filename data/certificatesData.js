4dfb6a9f4c900afc72399d88431cf416ddb906b9
ehsanrahnama <ehsanrahnama25@gmail.com>
2023-09-18 02:54:35 +0330

    add some multimedia file

---
+++ data/certificatesData.js
@@ -0,0 +1,18 @@
+import { v4 as uuidv4 } from "uuid";
+import {
+  FiFacebook,
+  FiInstagram,
+  FiLinkedin,
+  FiTwitter,
+  FiYoutube,
+} from "react-icons/fi";
+import certificate_ai from "../public/images/certificate_ai.JPG";
+
+export const certificatesData = [
+  {
+    title: "Generative AI with Large Language Models",
+    description:
+      "<a href=https://www.coursera.org/account/accomplishments/verify/Y3YSHD3M7Z9K?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course>DeepLearning.AI</a>",
+    img: certificate_ai,
+  },
+];
