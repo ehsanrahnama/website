7562cec34bc0137efa26617646c1f39316949b72
Ehsan.Aj <eng.ahanj@gmail.com>
2025-04-13 20:15:07 +0330

    bump to v1001

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // output: 'standalone', // Remove or change this
+  output: 'export', // Add this if you don't have it
+  // ... other configurations
+  unoptimized: true,
+  images: {
+    unoptimized: true,
+  },
+
   reactStrictMode: true,
-  output: 'export',
-}
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
