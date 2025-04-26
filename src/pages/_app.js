56328c31ae2d4d5311c2b11fd82eb5a11c8d3b63
Ehsan.Aj <eng.ahanj@gmail.com>
2025-04-26 18:05:37 +0330

    chnage font structure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,21 @@
 import DefaultLayout from "@/components/layout/DefaultLayout";
 import "@/styles/globals.css";
-import { AnimatePresence } from "framer-motion";
 import UseScrollToTop from '../hooks/useScrollToTop';
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
+    <div className={`${geistSans.className} ${geistMono.className} bg-secondary-light dark:bg-primary-dark transition duration-300`}>
       <DefaultLayout>
         <Component {...pageProps} />
       </DefaultLayout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,11 @@ import AppBanner from "@/components/shared/AppBanner";
 import { certificatesData } from "@/data/certificatesData";
 import { projectsData } from "@/data/projectsData";
 import { publicationsData } from "@/data/publicationsData";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
     <div
-      className={`${geistSans.className} ${geistMono.className} items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <div className="container mx-auto">
         <PagesMetaHead title="Home" />
