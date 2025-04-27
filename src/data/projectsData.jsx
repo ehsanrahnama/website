916850ce445143ff5e60405acc72a170f427ee14
EhsanRahi <ehsan.rahnama25@gmail.com>
2025-04-27 18:31:16 +0330

    added features

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -18,7 +18,7 @@ import housekeeping from "../../public/images/article1_localize.png";
 export const projectsData = [
   {
     title: "Develop HSE Assitance Based on LLM and RAG Architecture",
-    tags:  ["Python", "MongoDB", "AWS", "LangChain", "OpenAI"],
+    tags:  ["LangChain", "MongoDB Atlas", "AWS Lambda", "Amazon SageMaker"],
     description: (
       <div>
         <ul className="list-disc space-y-4">
@@ -26,11 +26,11 @@ export const projectsData = [
             Introduced RAG architecture to mitigate hallucination in the HSE
             Assistance system, enhancing the accuracy and reliability of the
             system’s responses.
-          </li>{"  "}
+          </li>
           <li>
             Designed and implemented a vector database on MongoDB Atlas to store
             and embed PDF ﬁles for eﬃcient retrieval and analysis.
-          </li>{"  "}
+          </li>
           <li>
             Utilized Amazon Lambda and Gateway API service to deploy a robust
             RESTful API, seamlessly integrating the HSE Assistance system with
@@ -49,20 +49,20 @@ export const projectsData = [
 
   {
     title: "Design Data and Test Platform for AI Teams",
-    tags:  ["ree", "dasf", "df", "sfgd", "fdsf"],
+    tags:  ["Python", "Flask", "PostgreSQL", "Docker", "Redash"],
     description: (
-      <>
+      <div>
         <ul className="list-disc space-y-4">
           <li>
             Developed a comprehensive data platform to manage, store, and
             organize labeled images and videos, enabling eﬃcient access and
             retrieval of dataset resources for AI model training.
-          </li>{" "}
+          </li>
           <li>
             Engineered a robust testing platform for rigorously evaluating AI
             models’ performance on diverse datasets, fostering consistent and
             reliable assessment across varying conditions.
-          </li>{" "}
+          </li>
           <li>
             Utilized Redash to create data visualizations that provided insights
             into complex datasets.
@@ -73,41 +73,43 @@ export const projectsData = [
             Github
           </a>
         </p> */}
-      </>
+      </div>
     ),
     img: testplatform,
   },
 
   {
     title: "Localize the Conceptual Difference for HSE Monitoring in Industrial Environments",
+    tags:  ["Tensorflow", "Pytorch", "Transfer Learning", "t-SNE", "OpenCV"],
     description: (
-      <>
+      <div>
         <ul className="list-disc space-y-4">
           <li>
           Developed a deep learning-based method using transfer learning and custom loss functions to detect and classify conceptual differences between industrial scene images.
-          </li>{" "}
+          </li>
           <li>
           Designed and labeled a specialized dataset, incorporating data augmentation techniques to address challenges such as lighting variations and object diversity.
-          </li>{" "}
+          </li>
           <li>
           Evaluated the model's performance on real industrial data, demonstrating its effectiveness and practical applicability for Health, Safety, and Environment (HSE) monitoring.
           </li>
         </ul>
         <p className="mt-6">
-          <a target="_blank" className="!font-bold  !text-blue-500" href="https://arxiv.org/abs/2208.04884">
+          <a target="_blank" className="!font-bold !text-blue-500" href="https://arxiv.org/abs/2208.04884">
             arXiv
           </a>
         </p>
-      </>
+      </div>
     ),
     img: housekeeping,
   },
 
   {
     title: "Quantized Yolov5 by Using Quantization-Aware-Training Methods",
+    tags:  ["Yolov5", "Tensorﬂow", "Quantization Aware Training"],
     description: (
       <>
-        <ul>
+        <ul  className="list-disc space-y-4">
           <li>
             Created TensorFlow equivalents for each layer in YOLOv5’s detection
             pipeline and replaced all PyTorch functions used in YOLOv5 with
@@ -123,8 +125,8 @@ export const projectsData = [
             trained quantized INT8 model for object detection on edge devices.
           </li>
         </ul>
-        <p>
-          <a href="https://github.com/ehsanrahnama/Quantization">Github</a>
+        <p className="mt-6">
+         <a target="_blank" className="!font-bold  !text-blue-500" href="https://github.com/ehsanrahnama/Quantization">Github</a>
         </p>
       </>
     ),
@@ -132,18 +134,19 @@ export const projectsData = [
   },
   {
     title: "Deploy Model on Embedded System Jetson NX",
+    tags: ["TensorRT", "tkDNN", "Pruning"],
     description: (
       <>
-        <ul>
+        <ul  className="list-disc space-y-4">
           <li>
             Converted YOLOv4 models to TensorRT format using the tkDNN framework
             to enable high-speed inference on embedded devices.
-          </li>{" "}
+          </li>
           <li>
             Pruned models and customized its architecture to meet specific
             performance and deployment constraints, improving both speed and
             resource efficiency.
-          </li>{" "}
+          </li>
           <li>
             Configured the NVIDIA Jetson Xavier NX board and installed all
             required dependencies for model deployment in a resource-constrained
@@ -156,18 +159,19 @@ export const projectsData = [
   },
   {
     title: "Train Object Detection Models by Custom Dataset",
+    tags: ["Keras", "Tensorflow", "Pytorch", "OpenCV", "Data Augmentation"],
     description: (
       <>
-        <ul>
+        <ul  className="list-disc space-y-4">
           <li>
             Applied transfer learning to fine-tune object detection models on
             domain-specific datasets, resulting in robust performance under
             dynamic and fluctuating surveillance conditions.
-          </li>{" "}
+          </li>
           <li>
             Collected, augmented, and labeled custom datasets tailored to
             specific project requirements, ensuring high-quality training data.
-          </li>{" "}
+          </li>
           <li>
             Integrated and deployed trained deep learning models into production
             environments using TensorFlow Serving and NVIDIA Triton Inference
@@ -179,23 +183,23 @@ export const projectsData = [
     ),
     img: Yolov5,
   },
-  // {
-  //   title: "Convert Model to Other Frameworks and Deploy in Production",
-  //   description: (
-  //     <>
-  //       <ul>
-  //         <li>Convert yolov4 to tensortRT by using tkDNN</li>
-  //         <li>Implement python code to convert yolov3 to tensorflow</li>{" "}
-  //         <li>
-  //           Export model to ONNX, torchscript and saved model, and
-  //           Tensorflowlite fromat{" "}
-  //         </li>
-  //         <li>
-  //           Depoly yolov5 on Nvidia Triton servers for using in production
-  //         </li>
-  //       </ul>
-  //     </>
-  //   ),
-  //   img: ConvertModel,
-  // },
+  {
+    title: "Convert Model to Other Frameworks and Deploy in Production",
+    description: (
+      <>
+        <ul>
+          <li>Convert yolov4 to tensortRT by using tkDNN</li>
+          <li>Implement python code to convert yolov3 to tensorflow</li>{" "}
+          <li>
+            Export model to ONNX, torchscript and saved model, and
+            Tensorflowlite fromat{" "}
+          </li>
+          <li>
+            Depoly yolov5 on Nvidia Triton servers for using in production
+          </li>
+        </ul>
+      </>
+    ),
+    img: ConvertModel,
+  },
 ];
--- src/pages/advantures/index.jsx
+++ src/pages/advantures/index.jsx
@@ -1,29 +1,37 @@
 import Link from "next/link";
 import PagesMetaHead from "../../components/PagesMetaHead";
+import northren_light_img  from "../../public/images/north_light1.JPG"; 
 
 
 function advantures() {
   return (
     <div className="container mx-auto">
       <PagesMetaHead title="Advantures" />
-      <Link href={"/advantures/northren_light"}>
-        <div className="mt-10 sm:mt-15 flex justify-center">
-          <h1 className="text-4xl font-bold mb-6 text-center text-green-700">
-            My Northern Lights Dream
-          </h1>
-          <p className="text-gray-700 text-lg mb-6">
-            Ever since I saw pictures of the Northern Lights on the internet,
-            I've really wanted to see them in real life. I often watch videos and
-            look at photos of them on Instagram. Wow! Every time, I feel so
-            surprised and happy.
-          </p>
-        </div>
-        <p className="text-gray-700 text-lg mb-6">
-          The colours are amazing, and the way the lights move and dance in the
-          night sky looks like magic. It's so beautiful. I told myself that one
-          day, I will travel and see these lights with my own eyes.
-        </p>
-      </Link>
+      <div className="mt-10 sm:mt-15 grid grid-cols-1 gap-6">
+        <Link href={"/advantures/northren_light"}>
+          <div className="border rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
+            <img
+              src="../../public/images/north_light1.JPG"
+              alt="Northern Lights"
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h1 className="text-2xl font-bold mb-2 text-green-700">
+                My Northern Lights Dream
+              </h1>
+              <p className="text-gray-700 text-sm">
+                Ever since I saw pictures of the Northern Lights on the internet,
+                I've really wanted to see them in real life. I often watch videos
+                and look at photos of them on Instagram. Wow! Every time, I feel
+                so surprised and happy. The colours are amazing, and the way the
+                lights move and dance in the night sky looks like magic. It's so
+                beautiful. I told myself that one day, I will travel and see these
+                lights with my own eyes.
+              </p>
+            </div>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
