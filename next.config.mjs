e34992680341ae3646864545eef2c0c89989e6e8
Ehsan.Aj <eng.ahanj@gmail.com>
2025-05-04 19:19:17 +0330

    some change 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
 
   reactStrictMode: true,
-
+  images: {
+    unoptimized: true,
+  },
   assetPrefix: '/website/',
 
 };
